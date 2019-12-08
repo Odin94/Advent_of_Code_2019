7b0a99bd5f128fac3dbe8905e32ba8099c3940c2
Andreas Kammerloher <kammerloher.andreas@gmail.com>
2019-12-08 14:10:54 +0100

    Solve day 05, part 1

--- 05/intcode.exs
+++ 05/intcode.exs
@@ -2,8 +2,6 @@ defmodule Intcode do
   @code_to_instruction_size %{1 => 4, 2 => 4, 3 => 2, 4 => 2, 99 => 1}
   @input 1
 
-  ##### TODO: Misunderstood how modes work, actually has up to 3 mode flags (remember, no leading 0s)
-  ##### Also see if you can use rem() to access opcodes or int > str > padleft > graphemes > Enu.at()
 
   def get_input() do
     File.read!("./input.txt")
@@ -19,15 +17,27 @@ defmodule Intcode do
     execute(code)
   end
 
-  def execute(code), do: execute(code, next_input(code, 0), 0)
+  def execute(code), do: fetch(code, next_input(code, 0), 0)
 
-  defp instruction_without_mode(code) when code > 100 do
-    code
-      |> Integer.to_string()
-      |> String.slice(-2..-1)
-      |> String.to_integer()
+  defp instruction_without_mode(instruction) do
+    instruction
+    |> Integer.to_string()
+    |> String.pad_leading(5, "0")
+    |> String.slice(-2..-1)
+    |> String.to_integer()
   end
-  defp instruction_without_mode(code), do: code
+
+  defp get_modes(instruction) do
+    instruction
+    |> Integer.to_string()
+    |> String.pad_leading(5, "0")
+    |> String.slice(0..-3)
+    |> String.graphemes()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp get_param(code, pos, 0), do: Map.get(code, pos)
+  defp get_param(_code, val, 1), do: val
 
   def execute(code, {1, a, b, pos}, i) do
     code = Map.put(code, pos, a + b)
@@ -50,7 +60,8 @@ defmodule Intcode do
     fetch(code, next_input(code, new_i), new_i)
   end
 
-  def execute(code, {4, output}, i) do
+  def execute(code, {4, output_pos}, i) do
+    IO.puts("Output: #{Map.get(code, output_pos)}")
     new_i = i + Map.get(@code_to_instruction_size, 3)
 
     fetch(code, next_input(code, new_i), new_i)
@@ -60,27 +71,38 @@ defmodule Intcode do
     Map.get(code, 0)
   end
 
-  def fetch(code, {instruction, a, b, pos}, i) when instruction > 1000 do
-    case instruction |> Integer.to_string() do
-      "11" <> _ -> execute(code, {instruction_without_mode(instruction), a, b, pos}, i)
-      "10" <> _ -> execute(code, {instruction_without_mode(instruction), Map.get(code, a), b, pos}, i)
-    end
-  end
+  def fetch(code, {instruction, a, b, pos}, i) do
+    modes = get_modes(instruction)
 
-  def fetch(code, {instruction, a, b, pos}, i) when instruction > 100 do
-    execute(code, {instruction_without_mode(instruction), a, Map.get(code, b), pos}, i)
+    execute(
+      code, {
+        instruction_without_mode(instruction),
+        get_param(code, a, Enum.at(modes, 2)),
+        get_param(code, b, Enum.at(modes, 1)),
+        pos
+      },
+      i
+    )
   end
 
   # for opcodes 3, 4
-  def fetch(code, {instruction, pos, a}, i) when instruction > 100 do
-    execute(code, {instruction_without_mode(instruction), Map.get(code, pos)}, i)
+  def fetch(code, {instruction, pos}, i) do
+    modes = get_modes(instruction)
+
+    execute(
+      code, {
+        instruction_without_mode(instruction),
+        pos
+      },
+      i
+    )
   end
 
-  def fetch(code, {instruction, a, b, pos}, i), do: execute(code, {instruction, Map.get(code, a), Map.get(code, b), pos}, i)
+  def fetch(code, {99}, i), do: execute(code, 99, i)
+
 
   defp next_input(code, new_i) do
     new_opcode = Map.get(code, new_i) |> instruction_without_mode()
-    IO.puts(new_opcode)
 
     (new_i)..(new_i + Map.get(@code_to_instruction_size, new_opcode) - 1)
     |> Enum.map(fn i -> Map.get(code, i) end)
