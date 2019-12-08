20bc14f318dd176983a26f50ceface77c22b0e5f
Andreas Kammerloher <kammerloher.andreas@gmail.com>
2019-12-08 11:04:25 +0100

    Solve day 04, part 2

--- 04/password.exs
+++ 04/password.exs
@@ -5,6 +5,7 @@ defmodule Password do
 
   def run do
     generate_valid_passwords(@min, @max)
+    |> Enum.count()
   end
 
   defp generate_valid_passwords(min, max) do
@@ -12,7 +13,6 @@ defmodule Password do
     |> to_six_digit_codes()
     |> with_adjacent_same_digits()
     |> without_decreasing_digits()
-    |> Enum.count()
   end
 
   defp to_six_digit_codes({min, max}) do
@@ -41,4 +41,29 @@ defmodule Password do
   defp has_no_decreasing_digits([_]), do: true
   defp has_no_decreasing_digits([a, b | _]) when a > b, do: false
   defp has_no_decreasing_digits([_head | tail]), do: has_no_decreasing_digits(tail)
+
+  # part 2
+
+  def run2 do
+    generate_valid_passwords(@min, @max)
+    |> without_only_adjacent_in_larger_group()
+    |> Enum.count()
+  end
+
+  defp without_only_adjacent_in_larger_group(codes) do
+    codes
+    |> Enum.filter(&has_adjacent_digits_in_small_group/1)
+  end
+
+  defp has_adjacent_digits_in_small_group([]), do: false
+  defp has_adjacent_digits_in_small_group([_]), do: false
+  defp has_adjacent_digits_in_small_group([a, a, a | tail]) do
+    case tail do
+       [^a | tail2] -> has_adjacent_digits_in_small_group([a, a, a | tail2])
+       [b | tail2] -> has_adjacent_digits_in_small_group(tail)
+       [] -> has_adjacent_digits_in_small_group([])
+    end
+  end
+  defp has_adjacent_digits_in_small_group([a, a | _]), do: true
+  defp has_adjacent_digits_in_small_group([a, b | tail]), do: has_adjacent_digits_in_small_group([b | tail])
 end
